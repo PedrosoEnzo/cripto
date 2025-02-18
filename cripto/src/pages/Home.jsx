5a7a1d5cb89373efafd68932fa0d4e92792faccd
pedrosoenzo <enzopedroso77@gmail.com>
2025-02-18 09:09:50 -0300

    Iniciando a tela inicial

--- cripto/src/pages/Home.jsx
+++ cripto/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import "./Home.css"; // Importação do CSS 
 
 function Home() {
-    return <h1>TESTE</h1>;
-   
+  return (
+    <div className="all-texto-inicial">
+      <div className="texto-inicial1">  
+        <h1>O futuro dos<br /> pagamentos é agora!</h1>
+      </div>
+
+      <div className="texto-inicial2">Simples, seguro e 100% cripto!</div>
+    </div>
+  );
 }
 
 export default Home;
-
