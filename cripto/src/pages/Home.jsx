f9b60e8e02f909d46e0af889a7f67faf506bb7f6
pedrosoenzo <enzopedroso77@gmail.com>
2025-02-23 20:03:37 -0300

    Fazendo rascunho da pagina inicial

--- cripto/src/pages/Home.jsx
+++ cripto/src/pages/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import "./Home.css"; // Importação do CSS 
+import "./Home.css"; // Importação do CSS
+import imagemFundoTeste from "../assets/imagem-fundo-teste.jpg"; // Caminho correto
+import imagemMoeda from "../assets/img-moeda.png";
 
 function Home() {
   return (
+    
     <div className="all-texto-inicial">
       <div className="texto-inicial1">  
         <h1>O futuro dos<br /> pagamentos é agora!</h1>
       </div>
-
       <div className="texto-inicial2">Simples, seguro e 100% cripto!</div>
+
+      <div className="imagem-fundo-inicial">
+
+      <div className="logo"></div>
+
+<div className="imgMoeda"></div>
+
+      </div>
     </div>
   );
 }
