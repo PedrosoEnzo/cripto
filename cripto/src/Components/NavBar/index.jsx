ea5420f08cf12dbe07bdf7d8092216f31adfc57f
Aninha-03 <anacbrito2007@gmail.com>
2025-03-06 07:57:53 -0300

    Foi criado um arquivo footer.jsx, dentro da pasta components

--- cripto/src/Components/NavBar/index.jsx
+++ cripto/src/Components/NavBar/index.jsx
@@ -4,12 +4,15 @@ import "./style.css";
 const Navbar = () => {
   return (
     <>
-      <div className="logo"></div>
+    <header>
+    <div className="logo"></div>
       <div className="navbar">
         <h2>Sobre</h2>
         <h2>Entrar</h2>
         <h2>Cadastre-se</h2>
       </div>
+    </header>
+    
     </>
   );
 };
