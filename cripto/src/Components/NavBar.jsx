ea834ca6914a1a5ea9648513ff7fa3243a3b6283
Mari1916 <mariana.freitas1916@gmail.com>
2025-03-13 10:33:03 -0300

    Tela inicial foi concluida

--- cripto/src/Components/NavBar.jsx
+++ cripto/src/Components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./style.css"; 
 import { Link } from "react-router-dom";
-// VERIFICAR !!! import logo from "../assets/logo.png";
+import ImagemLogo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
     <>
     <header> 
-      <img src="" alt="" />
+      
       <div className="navbar">
-
+      <img src={ImagemLogo} alt="Imagem da nossa logo em formatos de losangos" width={100}  className="imgLogo"/>
         <Link to="/ChainEducation" className="Link">
           ChainEducation
         </Link>
