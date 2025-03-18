67d61bb82fa0c591a9b76c16bb1055bfcf6a1ba6
Mari1916 <mariana.freitas1916@gmail.com>
2025-03-18 11:12:12 -0300

    Criado um css para pagina de cadastro

--- cripto/src/pages/Cadastro.jsx
+++ cripto/src/pages/Cadastro.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import "./Home.css"
+import "./cadastro.css"
 
-function Cadastro() {
+
+function Cadastro () {
   return (
     <div className="pagina-cadastro">
-      <h1>Bem vindo(a) a chainX</h1>
-      <p>Insira um e-mail que você costuma utilizar sempre. Será nele que os dados de acesso à sua conta chainX serão encaminhadas.</p>
-
-    </div>
+    <h1>Bem vindo(a) a chainX</h1>
+  </div>
   )
 
+}
 
-};
+
+
+
+
+
+
+export default Cadastro;
