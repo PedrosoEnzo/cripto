cfcf9fc70243a6bef469ba83c27e15ab5533e3c9
Mari1916 <mariana.freitas1916@gmail.com>
2025-04-08 10:39:15 -0300

    Adicionando olho para vizualização de senha

--- cripto-react/src/pages/Cadastro.jsx
+++ cripto-react/src/pages/Cadastro.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./cadastro.css";
+import olhos from "../assets/eyes.png";
 import Header from "../components/Navbar";
 
 const Cadastro = () => {
@@ -9,6 +10,7 @@ const Cadastro = () => {
   const [senha, setSenha] = useState("");
   const [confirmsenha, setConfirmsenha] = useState("");
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -52,10 +54,23 @@ const Cadastro = () => {
           <div>
             <input
               placeholder="Senha:"
-              type="password"
+              type={showPassword ? "text" : "password"} // Alterna entre oculto e visível
               value={senha}
               onChange={(e) => setSenha(e.target.value)}
               required
+            />
+              <img
+              src={olhos}
+              alt="Mostrar senha"
+              onClick={() => setShowPassword(!showPassword)}
+              style={{
+                position: "absolute",
+                right: "10px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                cursor: "pointer",
+                width: "20px",
+              }}
             />
           </div>
           <div>
