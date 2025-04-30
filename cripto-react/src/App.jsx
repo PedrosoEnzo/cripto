a92e08682c9096e72d83945a343510312b31213f
Aninha-3 <anacbrito2007@gmail.com>
2025-04-30 13:26:42 -0300

    Criando rota para a pagina de sobre

--- cripto-react/src/App.jsx
+++ cripto-react/src/App.jsx
@@ -7,7 +7,7 @@ import CoursePage from './components/CoursePage'
 import LessonPage from './components/LessonPage';
 import Simulador from './components/SimulatorPage';
 import ProfilePage from './components/ProfilePage';
-import About from './components/About'; // Importando o componente About
+import About from './components/About';
 
 
 
@@ -22,7 +22,7 @@ function App() {
         <Route path="/lesson/:id" element={<LessonPage />} />
         <Route path="/simulador" element={<Simulador />} />
         <Route path="/perfil" element={<ProfilePage />} />
-        <Route path="/about" element={<About />} /> {/* Página de sobre */}
+        <Route path="/about" element={<About />} />
     
       </Routes>
     </div>
