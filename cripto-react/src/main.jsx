2b03f3917e2b2401fa77cae84aa64a0ace5a011a
Aninha-3 <anacbrito2007@gmail.com>
2025-05-02 16:40:27 -0300

    Importanto fontes para a página de sobre

--- cripto-react/src/main.jsx
+++ cripto-react/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; 
-//import './index.css'; // Importando o CSS global
+import './index.css'; // Importando o CSS global
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
