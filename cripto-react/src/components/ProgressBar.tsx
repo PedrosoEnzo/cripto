83d5a19e0df87e6ed754769257200f829b79743f
Aninha-3 <anacbrito2007@gmail.com>
2025-05-05 09:44:04 -0300

    Colocando a porcentagem na barra de progresso

--- cripto-react/src/components/ProgressBar.tsx
+++ cripto-react/src/components/ProgressBar.tsx
@@ -1,9 +1,14 @@
 import styles from './ProgressBar.module.css';
 
 export default function ProgressBar({ progress }) {
+  const roundedProgress = Math.round(progress);
+
   return (
     <div className={styles.progressContainer}>
-      <div className={styles.progressBar} style={{ width: `${progress}%`}}></div>
+      <div className={styles.progressBar}
+        style={{ width: `${roundedProgress}%` }}
+      ></div>
+      <span className={styles.progressText}>{roundedProgress}%</span>
     </div>
   );
 }
