c4e10829b89b38c08a990bce6b510139c6200434
Aninha-3 <anacbrito2007@gmail.com>
2025-05-05 10:13:44 -0300

    Terminando o simulador da pagina Home

--- cripto-react/src/pages/Home.jsx
+++ cripto-react/src/pages/Home.jsx
@@ -27,66 +27,70 @@ export default function Home() {
       </header>
       <div className="navbar-img-moeda">
         <div className="content">
-          <h1 className="text">Simule, aprenda, <br /> invista no seu futuro!</h1>
+          <h1 className="text">Simule, Aprenda, <br /> Invista no seu futuro!</h1>
           <p>Se prepare para o futuro agora!</p>
         </div>
         <img src={moeda} alt="imgmoeda" className="moeda-image" />
       </div>
 
-      {/* Seção Frase Inspiradora */}
       <div className="frase">
         <h2>" O verdadeiro investidor não é aquele<br /> que tenta prever o futuro, mas aquele<br /> que se prepara para ele. "</h2>
         <p>- Benjamin Graham -</p>
       </div>
 
       <div className='plataformaCursos'>
-        <h2>A ChainX é uma plataforma de 
+        <h2>A ChainX é uma plataforma de
           <span className='highlight'> educação <br />
             financeira </span>onde você tem acesso a:</h2>
         <div className='plataformaCursosContent'>
           <div className='plataformaCursosCard'>
             <div className='plataformaCursosCardIcon'>
-            <img src={cursos} alt="cursos" />
-            <br />
-            <h3>Simulador de Investimentos</h3>
-            <p>Aprenda a investir com segurança e confiança.</p>
+              <img src={cursos} alt="cursos" />
+              <br />
+              <h3>Simulador de Investimentos</h3>
+              <p>Aprenda a investir com segurança e confiança.</p>
             </div>
           </div>
           <div className='plataformaCursosCard'>
-          <div className='plataformaCursosCardIcon'>
-            <img src={cursos} alt="cursos" />
-            <h3>Conteúdo Exclusivo</h3><br />
-            <p>Acesse cursos e materiais de alta qualidade.</p>
+            <div className='plataformaCursosCardIcon'>
+              <img src={cursos} alt="cursos" />
+              <h3>Conteúdo Exclusivo</h3><br />
+              <p>Acesse cursos e materiais de alta qualidade.</p>
             </div>
           </div>
           <div className='plataformaCursosCard'>
-          <div className='plataformaCursosCardIcon'>
-           <img src={cursos} alt="cursos" /> 
-            <h3>Comunidade Ativa</h3>
-            <br />
-            <p>Participe de discussões e trocas de experiências.</p>
-          </div>
+            <div className='plataformaCursosCardIcon'>
+              <img src={cursos} alt="cursos" />
+              <h3>Comunidade Ativa</h3>
+              <br />
+              <p>Participe de discussões e trocas de experiências.</p>
+            </div>
           </div>
         </div>
       </div>
 
       <div className='simulador'>
         <img src={simulador2} alt="simulador" className='simuladorImage' />
-        <h2>Simulador de <br />
-          <span className='hightLight'>Investimentos</span></h2>
-        <p>Aprenda a investir com segurança e confiança.</p>
-        <a href="/simulador">
-          <button className='simuladorButton'>Acessar</button>
-        </a>
-        <img src="" alt="" />
+
+        <div className="simuladorTexto">
+          <h2>
+            Simulador de <br />
+            <span className='hightLight'>Investimentos</span>
+          </h2>
+          <p>Aprenda a investir com segurança e confiança.</p>
+          <a href="/simulador">
+            <button className='simuladorButton'>Acessar</button>
+          </a>
+        </div>
       </div>
-        
+
+
 
       <footer>
         <div className="footerInfo">
           <div className="footerLink">
-            <a href="/chainxEduc">Chain Education</a>
-            <a href="/about">Sobre nós</a>
+            <a href="/curso">Chain Education</a>
+            <a href="/About">Sobre nós</a>
             <a href="#">Suporte</a>
           </div>
 
