fd70a0109c0a968a0018e7753149055199b000df
Mari1916 <mariana.freitas1916@gmail.com>
2025-05-08 10:56:10 -0300

    arrumando e finalizando o footer

--- cripto-react/src/components/Footer.tsx
+++ cripto-react/src/components/Footer.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
-import instagram from '../assets/icons/instagram.png';
-import facebook from '../assets/icons/facebook.png';
-import twitter from '../assets/icons/twitter.png';
+import "./Footer.css";
+import instagram from "../assets/icons/instagram.png";
+import facebook from "../assets/icons/facebook.png";
+import twitter from "../assets/icons/twitter.png";
+import logo from "../assets/logo.png";
 
 export default function Footer() {
-    return (
-        <>
-            <div className="footerInfo">
-                <div className="footerLink">
-                    <a href="/curso">Chain Education</a>
-                    <a href="/About">Sobre nós</a>
-                    <a href="#">Suporte</a>
-                </div>
+  return (
+    <>
+      <div className="footer">
+        <div className="footerInfo">
+          <img className="logo" src={logo} alt="Logo" width={90}></img>
+          <div className="detalhes">
+            <a href="/about">Sobre</a>
+            <a href="/perfil">Perfil</a>
+            <a href="/simulador">Simulador</a>
+            <a href="/curso">ChainX Education</a>
+          </div>
 
-                <div className="redesSociais">
-                    <a href="#"><img src={instagram} alt="Instagram" /></a>
-                    <a href="#"><img src={facebook} alt="Facebook" /></a>
-                    <a href="#"><img src={twitter} alt="Twitter" /></a>
-                </div>
-            </div>
+          <hr />
 
-            <div className="copy">
-                <p className= "copyT">&copy; Todos os direitos reservados a ChainX 2025</p>
-            </div>
-        </>
-    );
+          <div className="redesSociais">
+            <a href="#">
+              <img src={instagram} alt="Instagram" width={30} />
+            </a>
+            <a href="#">
+              <img src={facebook} alt="Facebook" width={30} />
+            </a>
+            <a href="#">
+              <img src={twitter} alt="Twitter" width={30} />
+            </a>
+          </div>
+        </div>
+
+        <div className="copy">
+          <p className="copyT">
+            &copy; Todos os direitos reservados a ChainX 2025
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
