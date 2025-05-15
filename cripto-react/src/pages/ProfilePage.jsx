992b42c1353a72908d6a08930f1cca79b1536f2b
Jumiranda28 <dri.kane@hotmail.com>
2025-05-15 13:27:13 -0300

    iniciando designer perfil

--- cripto-react/src/pages/ProfilePage.jsx
+++ cripto-react/src/pages/ProfilePage.jsx
@@ -1,27 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './ProfilePage.module.css'; // Corrigindo a importação
 
-<<<<<<< HEAD
-function ProfilePage ()  {
-  const userName = "Cassia Pedrosa";
-  const userBio = "Apaixonado por tecnologia e investimentos.";
-  const userEmail = "pedrosaCassia@gmail.com";
-  const userLevel = "Intermediário";
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.toggleh2}>Perfil</h2>
-      <div className={styles.profileContainer}>
-        <h2 className={styles.name}>{userName}</h2>
-        <br/>
-        <h3 className={styles.name}>{userEmail}</h3>
-        <p className={styles.bio}>{userBio}</p>
-        <div className={styles.levelSection}>
-          <h3 className={styles.levelTitle}>Nível: {userLevel}</h3>
-          <br />
-          <button classname={styles.btnEdit}>editar</button>
-        </div>
-      </div>
-=======
 function Perfil() {
   const LOCAL_STORAGE_KEY = 'perfilUsuario';
 
@@ -129,7 +108,6 @@ function Perfil() {
           <button type="button" className={`${styles.button} ${styles.cancelButton}`} onClick={handleCancelar}>Cancelar</button>
         </form>
       )}
->>>>>>> 2021a083f687286b52c9da7cae110fbb2cca7867
     </div>
   );
 }
